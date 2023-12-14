8e8e3e19ca77855931a867c07222e2bdbb978db9
gpopowic <gmpopowich@gmail.com>
2023-12-14 15:04:37 -0800

    Setup Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Portfolio from "./pages/portfolio/Portfolio";
+import FAQs from "./pages/FAQs/FAQs";
+import Resources from "./pages/resources/Resources";
+import NotFound from "./pages/notFound/NotFound";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Placeholder</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="FAQs" element={<FAQs />} />
+        <Route path="resources" element={<Resources />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
