10bfa8bcca4133b2a40422cb0b91c976d3ec9bf6
gpopowic <gmpopowich@gmail.com>
2023-12-19 13:24:25 -0800

    Added FAQ section

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,5 +1,21 @@
+import SectionHead from "./SectionHead";
+import { FaQuestion } from "react-icons/fa";
+import { faqs } from "../assets/data1";
+import FAQ from "./FAQ";
+
 const FAQs = () => {
-  return <div>FAQs</div>;
+  return (
+    <section className="faqs">
+      <div className="container faqs__container">
+        <SectionHead icon={<FaQuestion />} title="FAQs" />
+        <div className="faqs__wrapper">
+          {faqs.map(({ id, question, answer }) => {
+            return <FAQ key={id} question={question} answer={answer} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FAQs;
